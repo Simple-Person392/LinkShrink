a26e390e36e516af48e8227542838f35dbd67f21
Simple-Person392 <normalperson.8732@gmail.com>
2026-04-23 16:02:15 +0530

    GET/POST Endpoints

--- LinkShrinks/src/main/java/com/example/linkshrinks/Service/UrlService.java
+++ LinkShrinks/src/main/java/com/example/linkshrinks/Service/UrlService.java
@@ -1,11 +1,28 @@
 package com.example.linkshrinks.Service;
 
+import com.example.linkshrinks.entity.UrlMapping;
 import com.example.linkshrinks.repository.URLRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+@Service
 public class UrlService {
     private final URLRepository urlRepository;
 
     public UrlService(URLRepository urlRepository){
         this.urlRepository = urlRepository;
+    }
+    public UrlMapping createShortUrl(String originalUrl) {
+
+        String shortCode = UUID.randomUUID()
+
+                .toString()
+
+                .substring(0, 6);
+
+        UrlMapping urlMapping = new UrlMapping(originalUrl, shortCode);
+
+        return urlRepository.save(urlMapping);
+
     }
 }
--- LinkShrinks/src/main/java/com/example/linkshrinks/controller/UrlController.java
+++ LinkShrinks/src/main/java/com/example/linkshrinks/controller/UrlController.java
@@ -1,10 +1,15 @@
 package com.example.linkshrinks.controller;
 
+import com.example.linkshrinks.Service.UrlService;
+import com.example.linkshrinks.entity.UrlMapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-
 public class UrlController {
+    private final UrlService urlService;
+    public UrlController(UrlService urlService){
+        this.urlService = urlService;
+    }
 
     @GetMapping("/")
 
@@ -18,6 +23,13 @@ public class UrlController {
     public String shortener() {
 
         return "URL Shortener App is running 🚀";
+
+    }
+    @PostMapping("/shorten")
+
+    public UrlMapping shortenUrl(@RequestParam String originalUrl) {
+
+        return urlService.createShortUrl(originalUrl);
 
     }
 }
